7790531ba6b98cc6f04eb7da04da39af6f044017
Doug Waldron <douglas.waldron@dnr.ga.gov>
2022-12-22 12:59:31 -0500

    Add predicate builder and common entity filters

--- src/Domain/Entities/ISoftDelete.cs
+++ src/Domain/Entities/ISoftDelete.cs
@@ -3,17 +3,24 @@
 /// <summary>
 /// Marks an entity for enabling soft deletion in the database.
 /// </summary>
-public interface ISoftDelete<TUserKey>
-    where TUserKey : IEquatable<TUserKey>
+public interface ISoftDelete
 {
     bool IsDeleted { get; }
     DateTimeOffset? DeletedAt { get; }
+}
+
+/// <summary>
+/// Marks an entity for enabling soft deletion in the database and records the ID of the user who deleted the entity.
+/// </summary>
+public interface ISoftDelete<TUserKey> : ISoftDelete
+    where TUserKey : IEquatable<TUserKey>
+{
     TUserKey? DeletedBy { get; }
 
     /// <summary>
-    /// Sets the <see cref="IsDeleted"/> property to "true", indicating that the element should be considered to have
+    /// Sets the <see cref="ISoftDelete.IsDeleted"/> property to "true", indicating that the element should be considered to have
     /// been deleted. Also sets the <see cref="DeletedBy"/> property to the <see cref="userId"/> parameter value and
-    /// the <see cref="DeletedAt"/> property to the current <see cref="DateTimeOffset"/> value.
+    /// the <see cref="ISoftDelete.DeletedAt"/> property to the current <see cref="DateTimeOffset"/> value.
     /// </summary>
     /// <param name="userId">The ID of the user deleting the entity.</param>
     void SetDeleted(TUserKey? userId);
