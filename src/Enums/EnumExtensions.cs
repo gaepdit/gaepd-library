170169173d69ee75dcec7620098be10d3bb06812
Doug Waldron <douglas.waldron@dnr.ga.gov>
2023-08-10 10:57:26 -0400

    Reduce boxing in enum extensions

--- src/Enums/EnumExtensions.cs
+++ src/Enums/EnumExtensions.cs
@@ -13,14 +13,17 @@ public static class EnumExtensions
     /// Gets an attribute on an enum field value.
     /// </summary>
     /// <typeparam name="T">The type of the attribute to retrieve.</typeparam>
+    /// <typeparam name="TEnum">The Enum type of the enum value.</typeparam>
     /// <param name="enumValue">The enum value.</param>
+    /// <param name="enumString">The string representation of the enum value.</param>
     /// <returns>
     /// The attribute of the specified type or null.
     /// </returns>
-    private static T? GetAttributeOfType<T>(this Enum enumValue) where T : Attribute
+    private static T? GetAttributeOfType<T, TEnum>(this TEnum enumValue, string enumString)
+        where T : Attribute where TEnum : Enum
     {
         var type = enumValue.GetType();
-        var memInfo = type.GetMember(enumValue.ToString()).First();
+        var memInfo = type.GetMember(enumString)[0];
         var attributes = memInfo.GetCustomAttributes<T>(false);
         return attributes.FirstOrDefault();
     }
@@ -33,10 +36,11 @@ public static class EnumExtensions
     /// Use <see cref="DisplayAttribute"/> if exists.
     /// Otherwise, use the standard string representation.
     /// </returns>
-    public static string GetDisplayName(this Enum enumValue)
+    public static string GetDisplayName<TEnum>(this TEnum enumValue) where TEnum : Enum
     {
-        var attribute = enumValue.GetAttributeOfType<DisplayAttribute>();
-        return attribute?.Name ?? enumValue.ToString();
+        var enumString = enumValue.ToString();
+        var attribute = enumValue.GetAttributeOfType<DisplayAttribute, TEnum>(enumString);
+        return attribute?.Name ?? enumString;
     }
 
     /// <summary>
@@ -47,9 +51,10 @@ public static class EnumExtensions
     /// Use <see cref="DescriptionAttribute"/> if exists.
     /// Otherwise, use the standard string representation.
     /// </returns>
-    public static string GetDescription(this Enum enumValue)
+    public static string GetDescription<TEnum>(this TEnum enumValue) where TEnum : Enum
     {
-        var attribute = enumValue.GetAttributeOfType<DescriptionAttribute>();
-        return attribute?.Description ?? enumValue.ToString();
+        var enumString = enumValue.ToString();
+        var attribute = enumValue.GetAttributeOfType<DescriptionAttribute, TEnum>(enumString);
+        return attribute?.Description ?? enumString;
     }
 }
